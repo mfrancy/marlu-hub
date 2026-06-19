f09c1af48976c94729403c130c86e9193e230c26
Mariana <marianalopes697@gmail.com>
2026-06-19 00:02:39 -0300

    configura angular

---
+++ frontend/src/app/features/products/models/product.interface.ts
@@ -0,0 +1,16 @@
+export interface product {
+    id: string;
+    name: string;
+    code: string;
+    description: string;
+    brand: string;
+    category: string;
+    purchasePrice: number;
+    salePrice: number;
+    stockQuantity: number;
+    imageUrl: string;
+    isActive: boolean;
+    featured: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
