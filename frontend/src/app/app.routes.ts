691b741d62e3a00b2bfd28f52721d3e05a46b6be
Mariana <marianalopes697@gmail.com>
2026-08-04 18:09:28 -0300

    feat: rota e página criada

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 import { ProductList } from './features/products/pages/product-list/product-list';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { ProductForm } from './features/products/pages/product-form/product-form';
+import { RegisterUser } from './features/users/pages/register-user/register-user';
 
 export const routes: Routes = [
+   {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    component: RegisterUser
+  },
   {
     path: '',
     component: MainLayout,
---
+++ frontend/src/app/features/users/pages/register-user/register-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-user',
+  imports: [],
+  templateUrl: './register-user.html',
+  styleUrl: './register-user.scss',
+})
+export class RegisterUser {
+
+}
