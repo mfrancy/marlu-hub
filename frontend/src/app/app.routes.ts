adf4741a328af26c4a73f8636df18e69ecb8c4d1
Mariana <marianalopes697@gmail.com>
2026-08-06 11:16:07 -0300

    fix: apaga rota não utilizada

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ProductList } from './features/products/pages/product-list/product-list';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { ProductForm } from './features/products/pages/product-form/product-form';
-import { RegisterUser } from './features/users/pages/register-user/register-user';
 import { RegisterPage } from './features/auth/pages/register-page/register-page';
 import { LoginPage } from './features/auth/pages/login-page/login-page';
 
