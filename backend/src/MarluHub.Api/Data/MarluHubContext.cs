a64e7d80d1b4af86d4c9f8d570bee8c444f4ff42
Mariana <marianalopes697@gmail.com>
2026-08-06 12:43:27 -0300

    chore: configurando conexões do banco

---
+++ backend/src/MarluHub.Api/Data/MarluHubContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MarluHub.Api.Data;
+
+public class MarluHubContext : DbContext
+{
+
+    public MarluHubContext(DbContextOptions<MarluHubContext> options) : base(options) 
+    {
+        
+    }
+
+   
+
+}
--- backend/src/MarluHub.Api/Program.cs
+++ backend/src/MarluHub.Api/Program.cs
@@ -1,15 +1,23 @@
+using MarluHub.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<MarluHubContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
